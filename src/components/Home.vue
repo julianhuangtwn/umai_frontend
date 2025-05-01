7e514582ce344adbf4660c4464bf9eedd1dd0d04
julianhuangtwn <juliantwn@gmail.com>
2025-04-30 22:25:13 -0400

    Added delete button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,6 +13,10 @@
         }
     }
 
+    function deleteIngredient(i : string) {
+        ingredientList.value = ingredientList.value.filter((ingredient) => ingredient != i)
+    }
+
 </script>
 
 <template>
@@ -27,14 +31,14 @@
             <br>
             <div class="flex flex-row justify-center">
                 <input type="text" v-model="ingredient" placeholder="Enter your ingredient here:" class="h-10 w-96 pl-4 rounded-3xl">
-                <button @click="addIngredient" class="h-10 w-16 ml-4 rounded-lg border-2 bg-umai_green">+ Add</button>
+                <button @click="addIngredient" class="h-10 w-16 ml-4 rounded-lg border-2 text-white bg-umai_green">+ Add</button>
             </div>
             <h3 class="font-playfair text-xl justify-start text-white ml-6 mt-6">Your Ingredients:</h3>
             <div class="w-screen overflow-y-auto">
                 <div class="ml-4">
                     <ul class="flex flex-row flex-wrap">
                         <li v-for="i in ingredientList" :key="i" class="bg-umai_green rounded-lg p-2 m-2 text-white">
-                            <button class="font-bold text-red-600">X</button>
+                            <button @click="deleteIngredient(i)" class="font-bold text-red-600">X</button>
                             {{ i }}                 
                         </li>
                     </ul>
